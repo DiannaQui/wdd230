e10264c63a458db0579f1421612239dcfb2e8a6a
diannaqui <qui22001@byui.edu>
2022-10-21 14:48:42 -0600

    Lesson 6

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,20 @@
+const tC = 10;
+const skmH = 8.05;
+
+document.querySelector('.degrees').textContent = tC;
+document.querySelector('.speed').textContent = skmH;
+
+const tF = tC * (9/5) + 32;
+const smH = skmH / 1.609;
+
+console.log(tF);
+console.log(smH);
+
+if (tF <= 50 && smH > 3) { 
+const f = 35.74 + 0.6215 * tF - 35.75 * (smH**0.16) + 0.4275 * tF * (smH**0.16)
+
+document.querySelector('.wind').textContent = f.toFixed(2);
+}
+else {
+  document.querySelector('.wind').textContent = 'N/A';
+}
