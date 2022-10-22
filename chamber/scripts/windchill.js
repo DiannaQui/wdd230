61c721ed3f25032d741c6a83feefe12faef1a57c
diannaqui <qui22001@byui.edu>
2022-10-21 23:35:45 -0600

    Images updated

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -7,13 +7,9 @@ document.querySelector('.speed').textContent = skmH;
 const tF = tC * (9/5) + 32;
 const smH = skmH / 1.609;
 
-console.log(tF);
-console.log(smH);
-
 if (tF <= 50 && smH > 3) { 
-const f = 35.74 + 0.6215 * tF - 35.75 * (smH**0.16) + 0.4275 * tF * (smH**0.16)
-
-document.querySelector('.wind').textContent = f.toFixed(2);
+  const f = 35.74 + 0.6215 * tF - 35.75 * (smH**0.16) + 0.4275 * tF * (smH**0.16);
+  document.querySelector('.wind').textContent = f.toFixed(2);
 }
 else {
   document.querySelector('.wind').textContent = 'N/A';
