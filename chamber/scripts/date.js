63bd04a1dd657def4ae6187922437ee8629358a5
diannaqui <qui22001@byui.edu>
2022-10-22 19:41:48 -0600

    date

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,8 +1,7 @@
 document.querySelector(".date").textContent = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(new Date());
 
 document.querySelector('#year').textContent = new Date().getFullYear();
-
-document.querySelector('#lastupdated').textContent = `Last Modification: ${document.lastModified}`
+document.querySelector('#lastupdated').textContent = `Last Modification: ${document.lastModified}`;
 
 
 // ///////////// //
@@ -13,6 +12,6 @@ const day = new Date();
 const today = day.getDay();
 
 if (today < 1 || today > 2) {
-  const banner = document.querySelector('.joinUs');
-  banner.remove();
-}
+    const banner = document.querySelector('.joinUs');
+    banner.remove();
+} 
