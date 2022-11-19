427e185228b81aa97b48996d09d92a293e3e95a4
diannaqui <qui22001@byui.edu>
2022-11-19 11:26:41 -0700

    IF

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,7 +1,7 @@
 const weather = document.querySelector('.weatherInfo');
 
 // if (weather) {
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=766cc4c5847dc1e46a5927930e1b85f6';
+  // const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=766cc4c5847dc1e46a5927930e1b85f6';
 
   //////////////////////////
   //  Capitalize letters  //
@@ -94,7 +94,10 @@ const weather = document.querySelector('.weatherInfo');
 
   async function apiFetch() {
     try {
+      console.log('inside fetch beforeURL');
+      const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=766cc4c5847dc1e46a5927930e1b85f6';
       const response = await fetch(url);
+      console.log(response);
       if (response.ok) {
         const data = await response.json();
         console.log(data);
@@ -106,7 +109,10 @@ const weather = document.querySelector('.weatherInfo');
         console.log(error);
     }
   }
-
+  if (weather) {
+    // const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=766cc4c5847dc1e46a5927930e1b85f6';
+  console.log('inside');
   apiFetch();
+  }
 
 // }
