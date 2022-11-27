161b6dc8d25b6deb93b932a8113a9d392c284e1e
diannaqui <qui22001@byui.edu>
2022-11-26 17:27:10 -0700

    1st review

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,80 +1,42 @@
-// ////////////// //
-// Hamburger Menu //
-// ////////////// //
-
+// Hamburger
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
-  
   }
-  
-  const x = document.getElementById('hamburgerBtn')
+  const x = document.getElementById('hamburgerBtn');
   x.onclick = toggleMenu;
 
-
-// ////////////////// //
-// Time - Last Update //
-// ////////////////// //
-
-
+// Last Update
 document.querySelector(".date").textContent = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(new Date());
-
 document.querySelector('#year').textContent = new Date().getFullYear();
 document.querySelector('#lastupdated').textContent = `Last Modification: ${document.lastModified}`;
 
-
-// ///////////// //
-// Banner JoinUs //
-// ///////////// //
-
+// JoinUs
 const day = new Date();
 const today = day.getDay();
-
 if (today < 1 || today > 2) {
     const banner = document.querySelector('.joinUs');
-    banner.remove();
-} 
+    banner.remove(); } 
 
-
-// ////////////////////////////////////// //
-// Hidden date and time for the join form //
-// ////////////////////////////////////// //
-
+// Hidden date and time for the join form
 const joinDateCurrent = document.querySelector(".joinDate");
 if (joinDateCurrent) { joinDateCurrent.textContent = day }
 
-
-
-// ///////////// //
-//  LAST VISIT   //
-// ///////////// //
-
+// LastVisit
 let dayBefore = localStorage.getItem('date');
-
 localStorage.setItem('date', day);
-
 dayBefore = Date.parse(dayBefore);
-
 const number_mSdayS = (day - dayBefore)/86400000;
-
 
 if (!number_mSdayS) {
     const numDaysN = document.querySelector("#lastVisit");
-    if (numDaysN) { numDaysN.textContent = 0; }
-}
-
+    if (numDaysN) { numDaysN.textContent = 0; }}
 else {
     const numDaysY = document.querySelector("#lastVisit");
-    if (numDaysY) numDaysY.textContent = number_mSdayS.toFixed(0);
-}
+    if (numDaysY) numDaysY.textContent = number_mSdayS.toFixed(0);}
 
-
-// ///////////////////// //
-//  THANK YOU ELEMENTS   //
-// ///////////////////// //
-
+// Thanks
 const button = document.querySelector('.submitBtn');
-
 if (button) {
     button.addEventListener('click', () => {
         const nameF = document.querySelector('#fName').value;
@@ -92,98 +54,117 @@ const member = document.querySelector('.memberName');
 if (member) {
     member.textContent = `${localStorage.getItem('firstName')} ${localStorage.getItem('lastName')}`;
     localStorage.removeItem('firstName');
-    localStorage.removeItem('lastName');
-  }
+    localStorage.removeItem('lastName'); }
 
 const emailMember = document.querySelector('.emailConfirmation');
 if (emailMember) {
     emailMember.textContent = localStorage.getItem('emailM');
-    localStorage.removeItem('emailM');
-}
+    localStorage.removeItem('emailM'); }
 
 const phoneMember = document.querySelector('.phoneMember');
 if (phoneMember) {
     phoneMember.textContent = localStorage.getItem('phoneM');
-    localStorage.removeItem('phoneM');
-}
+    localStorage.removeItem('phoneM'); }
 
-
-// ///////////////////// //
-//       DIRECTORY       //
-// ///////////////////// //
-
+// Directory
 const displayDirectory = (dataDirectory) => {
     dataDirectory.companies.forEach ( 
         company => {
         let card = document.createElement('section');
-        let name = document.createElement('h3');
+        let name = document.createElement('h4');
         let logo = document.createElement('img');
         let address = document.createElement('p');
         let phoneNumber = document.createElement('p');
-
         let a = document.createElement('a');
         let link = document.createTextNode(company.URL);
         a.appendChild(link);
         a.href = company.URL;
         a.target = '_blank';
-
         name.textContent = company.name;
-
         logo.setAttribute('src', company.image);
         logo.setAttribute('alt', `Photo of ${company.name}`);
         logo.setAttribute('loading', 'lazy');
-  
         address.textContent = company.address;
         phoneNumber.textContent = company.phoneNumber;
-      
         card.appendChild(name);
         card.appendChild(logo);
         card.appendChild(address);
         card.appendChild(phoneNumber);
         card.appendChild(a);
-  
         document.querySelector('.gridArticle').appendChild(card);
       }
     )
   }
-  
+
+// Spotlight
+function shuffleCompanies(companies) {
+  let currentIndex = companies.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = companies[currentIndex];
+    companies[currentIndex] = companies[randomIndex];
+    companies[randomIndex] = temporaryValue;
+  } return companies;
+}
+
+function displaySpotlight(company,numberSpotlight) {
+    let card = document.createElement('section');
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let phoneNumber = document.createElement('p');
+    let a = document.createElement('a');
+    let link = document.createTextNode('WebSite');
+    a.appendChild(link);
+    a.href = company.URL;
+    a.target = '_blank';
+    logo.setAttribute('src', company.image);
+    logo.setAttribute('alt', `Photo of ${company.name}`);
+    logo.setAttribute('loading', 'lazy');
+    address.textContent = company.address;
+    phoneNumber.textContent = company.phoneNumber;
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(phoneNumber);
+    card.appendChild(a);
+    document.querySelector(`.spotlight${numberSpotlight}`).appendChild(card);}
+
+const shuffleSpotlight = (dataSpotlight) => {
+  const filterCompanies = dataSpotlight.companies.filter (( company ) => 
+        company.membershipLevel == 1 || company.membershipLevel == 2);
+        let filterCompaniesShuffle = shuffleCompanies(filterCompanies);
+        for (let i=0; i<3; i++) {
+          if (filterCompaniesShuffle[i]) {
+            displaySpotlight(filterCompaniesShuffle[i], i+1);
+          }    
+        }
+}
+        
 async function getDirectory() {
     const response = await fetch("./json/data.json");
-  
     if (response.ok) {
       let data = await response.json();
       console.log(data);
-      displayDirectory(data);
+      if (dir) {displayDirectory(data); }
+      if (spotL) {shuffleSpotlight(data); }
     }
   }
-  
   const dir = document.querySelector('.menuDirectory');
-  if (dir) {
-    getDirectory();
-  }
+  const spotL = document.querySelector('.spotlight');
+  if (dir || spotL) { getDirectory(); }
 
-
-
-// ///////////////////// //
-//      GRID - LIST      //
-// ///////////////////// //
-
+// GridList
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 
-
 if (gridbutton || listbutton) {
-
   const display = document.querySelector(".gridArticle");
-
   gridbutton.addEventListener("click", () => {
     display.classList.add("grid");
     display.classList.remove("list");
   });
-
   listbutton.addEventListener("click", () => {
     display.classList.add("list");
     display.classList.remove("grid");
   });
-
 }
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,118 +1,67 @@
 const weather = document.querySelector('.weatherInfo');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=rio rancho&units=imperial&appid=766cc4c5847dc1e46a5927930e1b85f6';
 
-
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=albuquerque&units=imperial&appid=766cc4c5847dc1e46a5927930e1b85f6';
-
-  //////////////////////////
-  //  Capitalize letters  //
-  //////////////////////////
-
-  function capital_letter(str) 
-  {
+// Capitalize
+  function capital_letter(str) {
       str = str.split(" ");
-
       for (var i = 0; i < str.length; i++) {
           str[i] = str[i][0].toUpperCase() + str[i].substr(1);
-      }
-
-      return str.join(" ");
+      } return str.join(" ");
   }
 
-
-  /////////////////////////
-  // Calculate WindChill //
-  /////////////////////////
-
+// WindChill
   function windChill(tF, smH) {
     const f = 35.74 + 0.6215 * tF - 35.75 * (smH**0.16) + 0.4275 * tF * (smH**0.16);
     return f;
   }
 
-
-  /////////////////////////
-  //       Display       //
-  /////////////////////////
-
-  function  displayResults(weatherData) {
-
+// Display
+  function displayResults(weatherData) {
     let weatherCard = document.createElement('section');
-    let weatherFirst = document.createElement('div');
+    let weatherFigure = document.createElement('figure');
     let weatherIcon = document.createElement('img');
-    let currentTemp = document.createElement('p');
     let captionDesc = document.createElement('figcaption');
+    let currentTemp = document.createElement('h3');
     let speed = document.createElement('p');
     let wind = document.createElement('p');
-    let lonlat = document.createElement('div');
-    let globe = document.createElement('img');
     let lonlatINT = document.createElement('div');
     let longitude = document.createElement('p');
     let latitude = document.createElement('p');
 
-    // WEATHER ICON
-
+// Icon
     const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
     const desc = weatherData.weather[0].description;
-
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', `Picture of ${desc}`);
     captionDesc.textContent = capital_letter(desc);
 
-    
-
-    // TEMPERATURE
-
+// Temperature
     const tF = weatherData.main.temp.toFixed(0);
-    currentTemp.innerHTML = `<strong>${tF}&deg;F</strong> `;
+    currentTemp.innerHTML = `${tF}&deg;F`;
 
-
-    // WIND CHILL 
-
+// WindChill
     const smH = weatherData.wind.speed;
-
     if (tF <= 50 && smH > 3) { 
       const windC = windChill(tF, smH);
       wind.textContent = `Wind Chill: ${windC.toFixed(2)}`;
-    }
-    
-    else {
-      wind.textContent = 'Wind Chill: N/A';
-    }
-
-    // SPEED
-
+    } else { wind.textContent = 'Wind Chill: N/A';}
     speed.innerHTML = `Wind Speed: ${smH}`;
 
-    // LONGITUDE - LATITUDE
-
-    const iconsrcLL = `images/globe.svg`;
-    const descLL = 'Image of the Earth';
-
-    latitude.textContent = `Lat: ${weatherData.coord.lat}`;
-    longitude.textContent = `Lon: ${weatherData.coord.lon}`;
-
-    globe.setAttribute('src', iconsrcLL);
-    globe.setAttribute('alt', `Picture of ${descLL}`);
-
+// Lon Lat
+    latitude.textContent = `Latitude: ${weatherData.coord.lat}`;
+    longitude.textContent = `Longitude: ${weatherData.coord.lon}`;
     lonlatINT.appendChild(longitude);
     lonlatINT.appendChild(latitude);
-    
-    lonlat.appendChild(lonlatINT);
-    lonlat.appendChild(globe);
-    lonlat.classList.add('lonlatGlobe');
-
-    weatherFirst.appendChild(weatherIcon);
-    weatherFirst.appendChild(currentTemp);
-    weatherFirst.classList.add('weatherTop');
-
-    weatherCard.appendChild(weatherFirst);
-    weatherCard.appendChild(captionDesc);
+    lonlatINT.classList.add('lonlatGlobe');
+    weatherFigure.appendChild(weatherIcon);
+    weatherFigure.appendChild(captionDesc)
+    weatherCard.appendChild(currentTemp);
+    weatherCard.appendChild(weatherFigure)
     weatherCard.appendChild(speed);
     weatherCard.appendChild(wind);
-    weatherCard.appendChild(lonlat);
-
+    weatherCard.appendChild(lonlatINT);
     document.querySelector('.weatherInfo').appendChild(weatherCard);
   }
-
 
   async function apiFetch() {
     try {
@@ -121,11 +70,7 @@ const weather = document.querySelector('.weatherInfo');
         const data = await response.json();
         console.log(data); 
         displayResults(data);
-      } else {
-          throw Error(await response.text());
-      }
-    } catch (error) {
-        console.log(error);
-    }
+      } else { throw Error(await response.text());}
+    } catch (error) { console.log(error);}
   }
 if (weather) { apiFetch(); }
